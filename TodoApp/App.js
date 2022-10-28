adafc17e5762282138744f051973824c27025054
yegri <exsilver305@gmail.com>
2022-10-28 18:02:32 +0900

    textinput, button UI, onPress 구현

--- TodoApp/App.js
+++ TodoApp/App.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import {SafeAreaView, StyleSheet} from 'react-native';
+import AddTodo from './components/AddTodo';
 import DateHead from './components/DateHead';
+import Empty from './components/Empty';
 
 function App() {
   const today = new Date();
 
   return (
-    <SafeAreaView>
+    <SafeAreaView style={styles.block}>
       <DateHead date={today} />
+      <Empty />
+      <AddTodo />
     </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  block: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+});
 
 export default App;
---
+++ TodoApp/components/Empty.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import {View, Text, Image, StyleSheet} from 'react-native';
+
+function Empty() {
+  return (
+    <View style={styles.block}>
+      <Image
+        source={require('../assets/images/young_and_happy.png')}
+        style={styles.image}
+        resizeMode="cover"
+      />
+      <Text style={styles.description}>야호! 할일이 없습니다.</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  block: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  image: {
+    width: 240,
+    height: 179,
+    marginBottom: 16,
+  },
+  description: {
+    fontSize: 24,
+    color: '9e9e9e',
+  },
+});
+
+export default Empty;
